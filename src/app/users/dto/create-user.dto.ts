acf9849b5dc1d7d3e60b610cbbad49348b9ae130
rafael <rafaelcnunes@icloud.com>
2023-02-23 13:31:22 -0300

    added regex helper and adjustments  in user module

--- src/app/users/dto/create-user.dto.ts
+++ src/app/users/dto/create-user.dto.ts
@@ -1,1 +1,19 @@
-export class CreateUserDto {}
+import { IsEmail, IsString, Matches, MaxLength } from 'class-validator';
+import { RegExHelper } from 'src/helpers/regex.helper';
+
+export class CreateUserDto {
+  @IsString()
+  @MaxLength(20)
+  firstName: string;
+
+  @IsString()
+  @MaxLength(50)
+  lastName: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @Matches(RegExHelper.password)
+  password: string;
+}
