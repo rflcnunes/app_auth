fab4087ab21ecaba0dc24ae458b48505a7275424
rafael <rafaelcnunes@icloud.com>
2023-02-23 12:22:31 -0300

    added user module and dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { User } from './entities/user.entity';
+import { UsersModule } from './app/users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD || process.env.DB_PASSWORD_ROOT,
+      database: process.env.DB_DATABASE,
+      entities: [User],
+      synchronize: process.env.NODE_ENV === 'development',
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
