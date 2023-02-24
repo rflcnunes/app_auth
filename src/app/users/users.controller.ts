19b88936d2565914d0d3f776142e22ca66178713
rafael <rafaelcnunes@icloud.com>
2023-02-24 01:57:20 -0300

    added jwt auth in users routes

--- src/app/users/users.controller.ts
+++ src/app/users/users.controller.ts
@@ -7,11 +7,14 @@ import {
   Param,
   Delete,
   ParseIntPipe,
+  UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
+@UseGuards(JwtAuthGuard)
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
