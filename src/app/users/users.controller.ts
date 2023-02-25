1e49807f15dbcaa7b6ca95c33460293b0acf8e0c
rafael <rafaelcnunes@icloud.com>
2023-02-25 01:52:28 -0300

    added audit methods in user crud

--- src/app/users/users.controller.ts
+++ src/app/users/users.controller.ts
@@ -11,10 +11,14 @@ import {
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { RabbitmqService } from '../../microservices/rabbitmq/rabbitmq.service';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly rabbitmqService: RabbitmqService,
+  ) {}
 
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
@@ -23,11 +27,28 @@ export class UsersController {
 
   @Get()
   findAll() {
+    this.rabbitmqService.sendMessage('audit', {
+      message: 'Find all users',
+      data: {
+        method: 'GET',
+        path: '/users',
+      },
+      action: 'findAll',
+    });
     return this.usersService.findAll();
   }
 
   @Get(':id')
   findOne(@Param('id', ParseIntPipe) id: number) {
+    this.rabbitmqService.sendMessage('audit', {
+      message: 'Find one user',
+      data: {
+        method: 'GET',
+        path: '/users/:id',
+        userId: id,
+      },
+      action: 'findOne',
+    });
     return this.usersService.findOne(id);
   }
 
@@ -36,11 +57,30 @@ export class UsersController {
     @Param('id', ParseIntPipe) id: number,
     @Body() updateUserDto: UpdateUserDto,
   ) {
+    this.rabbitmqService.sendMessage('audit', {
+      message: 'Update user',
+      data: {
+        method: 'Patch',
+        path: '/users/:id',
+        userId: id,
+        newValues: updateUserDto,
+      },
+      action: 'Update user',
+    });
     return this.usersService.update(id, updateUserDto);
   }
 
   @Delete(':id')
   remove(@Param('id', ParseIntPipe) id: number) {
+    this.rabbitmqService.sendMessage('audit', {
+      message: 'Delete user',
+      data: {
+        method: 'Delete',
+        path: '/users/:id',
+        userId: id,
+      },
+      action: 'Delete user',
+    });
     return this.usersService.remove(id);
   }
 }
--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -3,11 +3,12 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/entities/user.entity';
+import { RabbitmqService } from 'src/microservices/rabbitmq/rabbitmq.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, RabbitmqService],
   exports: [UsersService],
 })
 export class UsersModule {}
